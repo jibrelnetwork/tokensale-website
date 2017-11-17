fa8469e13655459aa9049042c4e406be5f13720f
Evgeny Erohin <erohin.em@gmail.com>
2017-11-17 20:45:14 +0300

    Verify Terms&Condition step updated

--- src/components/Auth/Verify/Terms.js
+++ src/components/Auth/Verify/Terms.js
@@ -6,13 +6,18 @@ import { set, compose, identity } from 'lodash/fp'
 import * as actions from '../../../actions'
 import { Input } from '../../common';
 
+/* eslint-disable max-len */
+const POLICY = 'I have read the Token Sale Terms & Conditions, Privacy Policy and Jibrel Network White Paper, and accept all terms, conditions, obligations, affirmations, representations and warranties outlined in these documents and agree to adhere to them and be legally bound by them'
+const CITIZENSHIP = 'I confirm that I am not citizen, permanent resident, or granted indefinite leave to remain in the US, Singapore or China - or any jurisdiction in which the purchase of Jibrel Network Token (JNT) is explicitly prohibited or outlawed.'
+/* eslint-enable */
+
 const Terms = ({ submitting, handleSubmit }) => (
   <div className="Terms">
     <form onSubmit={handleSubmit} className="form">
-      <Field name="policyConfirm" type="checkbox" component={Input} label="Policy text" />
-      <Field name="citizenshipConfirm" type="checkbox" component={Input} label="Citizenship text" />
-      <div className="submit">
-        <button type="submit" disabled={submitting}>Next Step</button>
+      <Field name="policyConfirm" type="checkbox" component={Input} label={POLICY} />
+      <Field name="citizenshipConfirm" type="checkbox" component={Input} label={CITIZENSHIP} />
+      <div className="buttons clear">
+        <button type="submit" className="button bordered pull-right" disabled={submitting}>Next Step</button>
       </div>
     </form>
   </div>
@@ -27,7 +32,7 @@ export default reduxForm({
   form: 'account',
   onSubmit: (_, dispatch) => dispatch(actions.auth.verify.confirmTerms()),
   validate: (values) => compose(
-    !values.policyConfirm ? set('policyConfirm', 'Policy agreement is required') : identity,
+    !values.policyConfirm ? set('policyConfirm', 'Policy agreement confirm is required') : identity,
     !values.citizenshipConfirm ? set('citizenshipConfirm', 'Citizenship confirmation is required') : identity,
   )({}),
   destroyOnUnmount: false,
