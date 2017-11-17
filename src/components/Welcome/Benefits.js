ed72a50ca9f757761897bb3f3a1c6d8647e9d74e
Evgeny Erohin <erohin.em@gmail.com>
2017-11-17 18:56:23 +0300

    ICO benefits component added

---
+++ src/components/Welcome/Benefits.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Benefits = () => (
+  <div className="section benefits">
+    <div className="inner">
+      <div className="row clear">
+        <div className="col-3 benefit-1">
+          <div className="img">
+            <img src="/static/icons/benefit-1.svg" alt="" />
+          </div>
+          <h3>USD $0.25</h3>
+          <p>Price of the token</p>
+        </div>
+        <div className="col-3 benefit-2">
+          <div className="img">
+            <img src="/static/icons/benefit-2.svg" alt="" />
+          </div>
+          <h3>Ethereum, Bitcoin</h3>
+          <p>Accepted currencies</p>
+        </div>
+        <div className="col-3 benefit-3">
+          <div className="img">
+            <img src="/static/icons/benefit-3.svg" alt="" />
+          </div>
+          <h3>Feb 1st 2017</h3>
+          <p>Tokens will be listed</p>
+        </div>
+        <div className="col-3 benefit-4">
+          <div className="img">
+            <img src="/static/icons/benefit-4.svg" alt="" />
+          </div>
+          <h3>USA, China, Singapore</h3>
+          <p>Forbidden countries</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Benefits
