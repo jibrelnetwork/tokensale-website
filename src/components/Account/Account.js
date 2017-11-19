5995f301f0e01fd4936ed7074c003d0c1768b668
Evgeny Erohin <erohin.em@gmail.com>
2017-11-19 21:08:04 +0300

    Account main component updated

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './Header'
+import Tokens from '../Welcome/Tokens'
+import Benefits from '../Welcome/Benefits'
+import Addresses from './Addresses'
+import Transactions from './Transactions'
 
 const Account = () => (
   <div className="Account">
-    Account
+    <div className="section start">
+      <div className="inner">
+        <Header />
+      </div>
+    </div>
+    <div className="section content">
+      <div className="inner">
+        <Addresses />
+        <Transactions />
+        <Tokens />
+      </div>
+    </div>
+    <Benefits />
   </div>
 )
 
