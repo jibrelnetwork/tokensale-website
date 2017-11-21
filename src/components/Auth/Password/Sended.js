d29db46ac0a50d1ea7eb3363fdd090fbe3adbf05
Evgeny Erohin <erohin.em@gmail.com>
2017-11-21 21:45:29 +0300

    Password recovery text fix

--- src/components/Auth/Password/Sended.js
+++ src/components/Auth/Password/Sended.js
@@ -7,7 +7,7 @@ const Sended = () => (
       <p>
         An email has been sent to your email address. <br />
         It can take a few minutes before you have it in your inbox. <br />
-        Click the recovery link to change your pawword!
+        Click the recovery link to change your password!
       </p>
     </div>
   </div>
