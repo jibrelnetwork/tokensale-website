ba86b4c4338207552964f38daecade452678dd98
Evgeny Erohin <erohin.em@gmail.com>
2017-11-22 00:33:45 +0300

    Raised tokens component updated

--- src/components/Welcome/Tokens.js
+++ src/components/Welcome/Tokens.js
@@ -2,12 +2,16 @@ import React from 'react'
 // import axios from 'axios'
 import numeral from 'numeral'
 import PropTypes from 'prop-types'
+import { compose } from 'lodash/fp'
+import { connect } from 'react-redux'
 import { lifecycle } from 'recompose'
+import * as actions from '../../actions'
+// import { SERVER } from '../../sagas'
 
 const Tokens = ({ raised, available }) => (
   <div className="Progress">
     <div className="progress">
-      <div className="line clear">
+      <div className="line clear" style={{ width: `${100 - ((raised / available) * 100)}%` }}>
         <div className="item raised">
           <div className="title">Raised Tokens</div>
           <div className="value">{numeral(raised).format('0,0')}</div>
@@ -24,26 +28,44 @@ const Tokens = ({ raised, available }) => (
 )
 
 Tokens.propTypes = {
-  raised: PropTypes.number,
+  raised: PropTypes.number.isRequired,
   available: PropTypes.number,
 }
 
 Tokens.defaultProps = {
-  raised: 12000000,
   available: 120000000,
 }
 
-const enhance = lifecycle({
-  state: { raised: undefined },
-  async componentDidMount() {
-    try {
-      // const response = await axios.get('https://example.com')
-      // const { raised } = await response.data.raised
-      // this.setState({ raised }) // eslint-disable-line react/no-did-mount-set-state, fp/no-this
-    } catch (error) {
-      console.log(`Raised tokens request error: ${error}`)
-    }
-  },
+const mapStateToProps = (state) => ({
+  raised: state.tokens.raised,
 })
+
+const mapDispatchToProps = {
+  getRaised: actions.tokens.raisedRequest,
+}
+
+const enhance = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  lifecycle({
+    componentDidMount() {
+      this.props.getRaised() // eslint-disable-line fp/no-this
+    },
+  })
+  // withState('raised', 'setRaised', 12000000),
+  // lifecycle({
+  //   async componentDidMount() {
+  //     try {
+  //       const response = await axios.get(`${SERVER}/api/raised-tokens/`)
+  //       const { raised } = response.data.raised_tokens
+  //       this.props.setRaised(raised) // eslint-disable-line react/no-did-mount-set-state, fp/no-this
+  //     } catch (error) {
+  //       console.log(`Raised tokens request error: ${error}`)
+  //     }
+  //   },
+  // })
+)
 
 export default enhance(Tokens)
