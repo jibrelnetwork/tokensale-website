422fb04aab4cc029a6ff6377e811f335e9603de9
Evgeny Erohin <erohin.em@gmail.com>
2017-11-21 13:17:07 +0300

    Email verification design fixes

--- src/sagas/auth/email.js
+++ src/sagas/auth/email.js
@@ -1,4 +1,4 @@
-import { push } from 'react-router-redux'
+import { replace } from 'react-router-redux'
 import { put, call, take } from 'redux-saga/effects'
 import * as EMAIL from '../../constants/auth/email'
 import request from '../request'
@@ -10,9 +10,9 @@ export function* verify() {
     const data = { key }
     const response = yield call(request, `${SERVER}/auth/registration/verify-email/`, data, 'post')
     if (response.success) {
-      yield put(push('/welcome/email/verified'))
+      yield put(replace('/welcome/email/verified'))
     } else {
-      yield put(push('/welcome/email/declined'))
+      yield put(replace('/welcome/email/declined'))
     }
   }
 }
