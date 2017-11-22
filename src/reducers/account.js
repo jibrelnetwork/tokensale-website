a881800d35ceb19bf8ef0e487400ff841f04f3dc
Evgeny Erohin <erohin.em@gmail.com>
2017-11-22 23:13:18 +0300

    Transactions mockups removed

--- src/reducers/account.js
+++ src/reducers/account.js
@@ -3,35 +3,7 @@ import * as ACCOUNT from '../constants/account'
 
 const defaultState = {
   balance: 0,
-  transactions: [{
-    jnt: 10000,
-    type: 'incoming',
-    date: '13:30 10/22/2017',
-    hash: '0x00360d2b7d240ec0643b6d819ba81a09e40e5bc1',
-    status: 'complete',
-    amount: '10 BTC / 72 000 USD',
-  }, {
-    jnt: 10000,
-    type: 'outgoing',
-    date: '13:30 10/22/2017',
-    hash: '0x00360d2b7d240ec0643b6d819ba81a09e40e5bc2',
-    status: 'complete',
-    amount: '10 BTC / 72 000 USD',
-  }, {
-    jnt: 10000,
-    type: 'outgoing',
-    date: '13:30 10/22/2017',
-    hash: '0x00360d2b7d240ec0643b6d819ba81a09e40e5bc3',
-    status: 'waiting',
-    amount: '10 BTC / 72 000 USD',
-  }, {
-    jnt: 10000,
-    type: 'incoming',
-    date: '13:30 10/22/2017',
-    hash: '0x00360d2b7d240ec0643b6d819ba81a09e40e5bc4',
-    status: 'waiting',
-    amount: '10 BTC / 72 000 USD',
-  }],
+  transactions: [],
 }
 
 const accountReducer = (state = defaultState, action) => {
