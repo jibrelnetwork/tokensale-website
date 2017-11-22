565bb6ef13a12f2a9e877b1e789d3ef56231ebc0
Evgeny Erohin <erohin.em@gmail.com>
2017-11-22 22:15:13 +0300

    Balance get saga converted to periodical

--- src/sagas/account/balance.js
+++ src/sagas/account/balance.js
@@ -1,18 +1,19 @@
-import { put, call, take } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { put, call } from 'redux-saga/effects'
 import * as actions from '../../actions'
-import * as BALANCE from '../../constants/account/transactions'
 import request from '../request'
 import { SERVER } from '../.'
 
+const DELAY = 60000
+
+// sync
 export function* get() {
   while (true) { // eslint-disable-line fp/no-loops
-    yield take(BALANCE.REQUEST)
     const response = yield call(request, `${SERVER}/api/account/`, null, 'get')
     if (response.success) {
       const { balance = 0 } = response.data.jnt_balance
       yield put(actions.account.balance.requestSuccess(balance))
-    } else {
-      alert('Balance request error')
-    }
+    } else { console.log('Balance request error') }
+    yield call(delay, DELAY)
   }
 }
