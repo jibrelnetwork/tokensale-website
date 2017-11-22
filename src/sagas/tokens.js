373eddf50ce5b5cdf9c68a1c39d6d50eef6a3f08
Evgeny Erohin <erohin.em@gmail.com>
2017-11-22 22:32:24 +0300

    Raised tokens saga converted to periodical

--- src/sagas/tokens.js
+++ src/sagas/tokens.js
@@ -1,16 +1,19 @@
-import { put, call, take } from 'redux-saga/effects'
+import { put, call } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
 import * as actions from '../actions'
-import * as TOKENS from '../constants/tokens'
 import request from './request'
 import { SERVER } from '.'
 
+const DELAY = 300000
+
+// synced
 export function* getRaised() {
   while (true) { // eslint-disable-line fp/no-loops
-    yield take(TOKENS.RAISED_REQUEST)
     const response = yield call(request, `${SERVER}/api/raised-tokens/`, null, 'get')
     if (response.success) {
       const tokens = response.data.raised_tokens
       yield put(actions.tokens.raisedRequestSuccess(tokens))
-    }
+    } else { console.log('Raised tokens request error') }
+    yield call(delay, DELAY)
   }
 }
