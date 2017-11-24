6bc03934fdbc790242d08e42338a2a83b610069a
Evgeny Erohin <erohin.em@gmail.com>
2017-11-24 11:33:15 +0300

    Get verification status request fail fix

--- src/sagas/auth/verify.js
+++ src/sagas/auth/verify.js
@@ -16,21 +16,16 @@ const DELAY = 30000
 export function* getStatus(periodically = false) {
   function* makeRequest() {
     const response = yield call(request, `${SERVER}/api/account/`, null, 'get')
-
     if (response.success) {
       const status = response.data.identity_verification_status
       const isVerified = response.data.is_identity_verified
-
-      if (isVerified) {
-        gtm.pushRegistrationSuccess()
-      }
-
+      if (isVerified) { gtm.pushRegistrationSuccess() }
       yield put(actions.auth.verify.setStatus(status || 'Pending')) // ?
     } else {
+      yield put(actions.auth.verify.setStatus('Pending'))
       console.log('Verification request error')
     }
   }
-
   if (periodically) {
     while (true) { // eslint-disable-line fp/no-loops
       yield call(makeRequest)
@@ -44,19 +39,13 @@ export function* getStatus(periodically = false) {
 export function* confirmTerms() {
   while (true) { // eslint-disable-line fp/no-loops
     yield take(VERIFY.CONFIRM_TERMS)
-
     const form = yield select(getForm)
     const data = { terms_confirmed: true }
-
     yield put(startSubmit(form))
-
     const response = yield call(request, `${SERVER}/api/account/`, data, 'put')
-
     if (response.success) {
       yield put(stopSubmit(form))
-
       gtm.pushVerificationNextStep('ConfirmAgreement')
-
       yield put(actions.auth.verify.setStage('user-info'))
     } else {
       yield put(stopSubmit(form))
@@ -69,9 +58,7 @@ export function* updateUserInfo() {
     const {
       payload: { firstName, lastName, birthday, residency, citizenship },
     } = yield take(VERIFY.UPDATE_USER_INFO)
-
     const form = yield select(getForm)
-
     const data = {
       residency,
       last_name: lastName,
@@ -79,16 +66,11 @@ export function* updateUserInfo() {
       citizenship,
       date_of_birth: moment(birthday).format('YYYY-MM-DD'),
     }
-
     yield put(startSubmit(form))
-
     const response = yield call(request, `${SERVER}/api/account/`, data, 'put')
-
     if (response.success) {
       yield put(stopSubmit(form))
-
       gtm.pushVerificationNextStep('BasicInfo')
-
       yield put(actions.auth.verify.setStage('document'))
     } else if (response.error) {
       const errors = {
@@ -98,7 +80,6 @@ export function* updateUserInfo() {
         residency: response.data.residency,
         citizenship: response.data.citizenship,
       }
-
       yield put(stopSubmit(form, errors))
     } else {
       yield put(stopSubmit(form))
@@ -111,16 +92,11 @@ export function* uploadDocument() {
     const { payload: { documentUrl } } = yield take(VERIFY.UPLOAD_DOCUMENT)
     const form = yield select(getForm)
     const data = { document_url: documentUrl }
-
     yield put(startSubmit(form))
-
     const response = yield call(request, `${SERVER}/api/account/`, data, 'put')
-
     if (response.success) {
       yield put(stopSubmit(form))
-
       gtm.pushVerificationNextStep('PassportScan')
-
       yield put(actions.auth.verify.setStatus('Pending'))
       yield put(actions.auth.verify.setStage('loader'))
       yield delay(25000)
