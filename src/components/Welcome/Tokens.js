794ece06a8833b89e4514a31d225ed337246252b
Ivan Violentov <violentoff@gmail.com>
2017-11-27 21:01:19 +0300

    Resolve #25

--- src/components/Welcome/Tokens.js
+++ src/components/Welcome/Tokens.js
@@ -1,20 +1,20 @@
 import React from 'react'
-// import axios from 'axios'
 import numeral from 'numeral'
 import PropTypes from 'prop-types'
 import { compose } from 'lodash/fp'
 import { connect } from 'react-redux'
 import { lifecycle } from 'recompose'
+
 import * as actions from '../../actions'
-// import { SERVER } from '../../sagas'
 
 const AVAILABLE = 200000000
 
-const Tokens = ({ raised }) => (
+const Tokens = ({ raised, raisedPercent }) => (
   <div className="Progress">
     <div className="progress">
       <div className="wrap">
-        <div className="line clear" style={{ width: `${100 - ((raised / AVAILABLE) * 100)}%` }}>
+        <div className="line before" style={{ width: `${raisedPercent + 1}%` }} />
+        <div className="line after clear" style={{ width: `${100 - raisedPercent}%` }}>
           <div className="item raised">
             <div className="title">Raised Tokens</div>
             <div className="value">{numeral(raised).format('0,0')}</div>
@@ -23,7 +23,7 @@ const Tokens = ({ raised }) => (
         </div>
         <div className="item total">
           <div className="title">Available Tokens</div>
-          <div className="value">{numeral(AVAILABLE - raised).format('0,0')}</div>
+          <div className="value">{numeral(AVAILABLE).format('0,0')}</div>
           <div className="point" />
         </div>
       </div>
@@ -33,10 +33,12 @@ const Tokens = ({ raised }) => (
 
 Tokens.propTypes = {
   raised: PropTypes.number.isRequired,
+  raisedPercent: PropTypes.number.isRequired,
 }
 
-const mapStateToProps = (state) => ({
-  raised: state.tokens.raised,
+const mapStateToProps = ({ tokens: { raised } }) => ({
+  raised,
+  raisedPercent: (raised / AVAILABLE) * 100,
 })
 
 const mapDispatchToProps = {
@@ -55,18 +57,6 @@ const enhance = compose(
     componentWillUnmount() { this.props.cancelRequest() },
     /* eslint-disable fp/no-this */
   })
-  // withState('raised', 'setRaised', 12000000),
-  // lifecycle({
-  //   async componentDidMount() {
-  //     try {
-  //       const response = await axios.get(`${SERVER}/api/raised-tokens/`)
-  //       const { raised } = response.data.raised_tokens
-  //       this.props.setRaised(raised) // eslint-disable-line react/no-did-mount-set-state, fp/no-this
-  //     } catch (error) {
-  //       console.log(`Raised tokens request error: ${error}`)
-  //     }
-  //   },
-  // })
 )
 
 export default enhance(Tokens)
