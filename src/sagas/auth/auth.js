e10648cc16eb8cd6b5b143e5e4b72f6d41f2dc41
Evgeny Erohin <erohin.em@gmail.com>
2017-11-29 21:43:39 +0300

    is_identity_verified param added to user login saga

--- src/sagas/auth/auth.js
+++ src/sagas/auth/auth.js
@@ -15,8 +15,8 @@ function* getUserData(token) {
   const response = yield call(request, `${SERVER}/api/account/`, null, 'get', token)
   if (response.success) {
     const data = {
-      verifyStatus: response.data.identity_verification_status || 'Pending', // ?
       isVerified: response.data.is_identity_verified,
+      verifyStatus: response.data.identity_verification_status || 'Pending', // ?
       isTermsConfirmed: response.data.terms_confirmed,
       isUserInfoFilled:
         !!response.data.first_name &&
@@ -24,13 +24,15 @@ function* getUserData(token) {
         !!response.data.date_of_birth &&
         !!response.data.citizenship &&
         !!response.data.residency,
-      idDocumentUploaded: !!response.data.document_url,
+      isDocumentUploaded: !!response.data.document_url,
     }
-    if (!data.isTermsConfirmed) {
+    if (data.isVerified) {
+      yield put(actions.auth.verify.setStatus(data.verifyStatus))
+    } else if (!data.isTermsConfirmed) {
       yield put(actions.auth.verify.setStage('terms'))
     } else if (!data.isUserInfoFilled) {
       yield put(actions.auth.verify.setStage('user-info'))
-    } else if (!data.idDocumentUploaded) {
+    } else if (!data.isDocumentUploaded) {
       yield put(actions.auth.verify.setStage('document'))
     } else {
       yield put(actions.auth.verify.setStatus(data.verifyStatus))
