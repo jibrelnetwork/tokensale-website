d4b65c888ccf06b3d12f60f275caecf9df4835ec
Evgeny Erohin <erohin.em@gmail.com>
2017-11-29 20:52:56 +0300

    Timer component translated

--- src/components/Welcome/Timer.js
+++ src/components/Welcome/Timer.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import moment from 'moment'
 import PropTypes from 'prop-types'
+import { compose } from 'lodash/fp'
 import { lifecycle } from 'recompose'
+import { translate } from 'react-i18next'
 
-const Timer = ({ timeLeft }) => (
+const Timer = ({ t, timeLeft }) => (
   <div className="Timer">
     <div className="counter">
       <div className="row clear">
         <div className="col-3">
           <div className="time">{timeLeft.days() + 31}</div>
-          <div className="title">Days</div>
+          <div className="title">{t('index.timer.days')}</div>
         </div>
         <div className="col-3">
           <div className="time">{timeLeft.hours() > 0 ? timeLeft.hours() : 0 }</div>
-          <div className="title">Hours</div>
+          <div className="title">{t('index.timer.hours')}</div>
         </div>
         <div className="col-3">
           <div className="time">{timeLeft.minutes() > 0 ? timeLeft.minutes() : 0 }</div>
-          <div className="title">Minutes</div>
+          <div className="title">{t('index.timer.minutes')}</div>
         </div>
         <div className="col-3">
           <div className="time">{timeLeft.seconds() > 0 ? timeLeft.seconds() : 0 }</div>
-          <div className="title">Seconds</div>
+          <div className="title">{t('index.timer.seconds')}</div>
         </div>
       </div>
     </div>
@@ -29,32 +31,36 @@ const Timer = ({ timeLeft }) => (
 )
 
 Timer.propTypes = {
+  t: PropTypes.func.isRequired,
   timeLeft: PropTypes.object.isRequired,
 }
 
-const enhance = lifecycle({
-  state: { timeLeft: moment.duration(moment.utc('2018-01-26T12:00') - moment.utc()) },
-  /* eslint-disable fp/no-this */
-  componentWillMount() {
-    const { timeLeft } = this.state
-    if (timeLeft.asMilliseconds() > 0) {
-      this.timer = setInterval(
-        () => this.setState({
-          timeLeft: timeLeft.subtract(1, 'seconds'),
-        }),
-        1000
-      )
-    }
-  },
-  componentWillUnmount() {
-    clearInterval(this.timer)
-  },
-  componentDidUpdate() {
-    if (this.state.timeLeft.asMilliseconds() < 0) {
+const enhance = compose(
+  translate(),
+  lifecycle({
+    state: { timeLeft: moment.duration(moment.utc('2018-01-26T12:00') - moment.utc()) },
+    /* eslint-disable fp/no-this */
+    componentWillMount() {
+      const { timeLeft } = this.state
+      if (timeLeft.asMilliseconds() > 0) {
+        this.timer = setInterval(
+          () => this.setState({
+            timeLeft: timeLeft.subtract(1, 'seconds'),
+          }),
+          1000
+        )
+      }
+    },
+    componentWillUnmount() {
       clearInterval(this.timer)
-    }
-  },
-/* eslint-enable */
-})
+    },
+    componentDidUpdate() {
+      if (this.state.timeLeft.asMilliseconds() < 0) {
+        clearInterval(this.timer)
+      }
+    },
+  /* eslint-enable */
+  })
+)
 
 export default enhance(Timer)
