bc6a3aaf00701394dffeec24e28924fe1193448e
Evgeny Erohin <erohin.em@gmail.com>
2017-11-30 16:09:17 +0300

    Verification header translated

--- src/components/Auth/Verify/Header.js
+++ src/components/Auth/Verify/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { compose } from 'lodash/fp'
+import { connect } from 'react-redux'
+import { translate } from 'react-i18next'
+
 import * as actions from '../../../actions'
 
-const Header = ({ logout }) => (
+const Header = ({ t, logout }) => (
   <div className="Header">
     <div className="header clear">
       <Link to="/welcome" className="logo pull-left">
@@ -12,7 +15,7 @@ const Header = ({ logout }) => (
       </Link>
       <ul className="menu pull-right">
         <li className="bordered">
-          <button onClick={logout}>Logout</button>
+          <button onClick={logout}>{t('verification.logout')}</button>
         </li>
       </ul>
     </div>
@@ -20,6 +23,7 @@ const Header = ({ logout }) => (
 )
 
 Header.propTypes = {
+  t: PropTypes.func.isRequired,
   logout: PropTypes.func.isRequired,
 }
 
@@ -27,7 +31,10 @@ const mapDispatchToProps = {
   logout: actions.auth.logout,
 }
 
-export default connect(
-  undefined,
-  mapDispatchToProps,
+export default compose(
+  translate(),
+  connect(
+    undefined,
+    mapDispatchToProps,
+  )
 )(Header)
