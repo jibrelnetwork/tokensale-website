80d1b53715d0847954eaad041794020aa12a05dd
Evgeny Erohin <erohin.em@gmail.com>
2017-12-03 17:35:56 +0300

    Skip document upload button added #49

--- src/components/Auth/Verify/Document.js
+++ src/components/Auth/Verify/Document.js
@@ -1,35 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { compose, noop } from 'lodash/fp'
+import { compose } from 'lodash/fp'
+import { withState, withHandlers } from 'recompose'
 import { Field, reduxForm } from 'redux-form'
-import { withState, withProps, lifecycle } from 'recompose'
 
-import { Uploader } from '../../common'
 import * as actions from '../../../actions'
+import { Uploader, Modal } from '../../common'
 
-const Document = ({ setStage, submitting, handleSubmit, isTimeLeft, invalid }) => (
+const Document = ({
+  skip,
+  setStage,
+  submitting,
+  handleSubmit,
+  openSkipModal,
+  closeSkipModal,
+  isSkipModalOpen,
+}) => (
   <div className="Identification">
+    <Modal
+      isOpen={isSkipModalOpen}
+      onClose={closeSkipModal}
+    >
+      <div>
+        Verification is required to withdraw your JNT. You can complete this step at a later stage.
+      </div>
+      <div className="buttons clear">
+        <button
+          onClick={skip}
+          className="button bordered pull-right"
+        >
+          Upload later
+        </button>
+        <button
+          onClick={closeSkipModal}
+          disabled={submitting}
+          className="button bordered"
+        >
+          Upload now
+        </button>
+      </div>
+    </Modal>
     <form onSubmit={handleSubmit} className="form">
       <Field name="document" component={Uploader} />
-      {isTimeLeft && invalid && (
-        <div className="info">
-          Having trouble uploading your KYC documentation?
-          Verification is required to withdraw your JNT.
-          You can complete this step at a later stage.
-        </div>
-      )}
       <div className="buttons clear">
         <button
           type="submit"
           disabled={submitting}
           className="button bordered pull-right"
         >
-          {!submitting && (
-            isTimeLeft && invalid
-              ? 'Skip for now'
-              : 'Next Step'
-          )}
+          Next Step
+        </button>
+        <button
+          style={{ marginRight: 5 }}
+          onClick={openSkipModal}
+          className="button bordered pull-right"
+        >
+          Skip for now
         </button>
         <button
           onClick={() => setStage('user-info')}
@@ -44,11 +71,13 @@ const Document = ({ setStage, submitting, handleSubmit, isTimeLeft, invalid }) =
 )
 
 Document.propTypes = {
-  invalid: PropTypes.bool.isRequired,
+  skip: PropTypes.func.isRequired,
   setStage: PropTypes.func.isRequired,
   submitting: PropTypes.bool.isRequired,
-  isTimeLeft: PropTypes.bool.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  openSkipModal: PropTypes.func.isRequired,
+  closeSkipModal: PropTypes.func.isRequired,
+  isSkipModalOpen: PropTypes.bool.isRequired,
 }
 
 const mapStateToProps = (state) => ({
@@ -56,6 +85,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
+  skip: actions.auth.verify.skipDocument,
   setStage: actions.auth.verify.setStage,
 }
 
@@ -63,51 +93,32 @@ const mapDispatchToProps = {
 
 export default compose(
   withState(
-    'timer',
-    'updateTimer',
-    8,
+    'isSkipModalOpen',
+    'toggleSkipModal',
+    false,
   ),
-  lifecycle({
-    componentWillMount() {
-      this.timer = setInterval(
-        () => this.props.updateTimer((timer) => timer - 1),
-        1000
-      )
-    },
-    componentWillUnmount() {
-      clearInterval(this.timer)
-    },
-    componentDidUpdate() {
-      if (this.props.timer === 0) {
-        clearInterval(this.timer)
-      }
-    },
+  withHandlers({
+    openSkipModal: (props) =>
+      () => props.toggleSkipModal(true),
+    closeSkipModal: (props) =>
+      () => props.toggleSkipModal(false),
   }),
-  withProps(
-    ({ timer }) => ({ isTimeLeft: timer === 0 }),
-  ),
   connect(
     mapStateToProps,
     mapDispatchToProps,
   ),
   reduxForm({
-    onSubmit: ({ document }, dispatch, { isTimeLeft }) =>
-      document.url && document.type
-        ? dispatch(actions.auth.verify.uploadDocument(document.url, document.type))
-        : isTimeLeft
-          ? dispatch(actions.auth.verify.skipDocument())
-          : noop,
-    validate: ({ document: { url, type } = {} }, props) =>
-      (!url || !type) && !props.isTimeLeft
-        ? { document: 'Upload document scan in order to verify your identity' }
-        : {},
+    onSubmit: ({ document: { url, type } }, dispatch) =>
+      dispatch(actions.auth.verify.uploadDocument(url, type)),
+    validate: ({ document: { url, type } = {} }) => !url || !type
+      ? { document: 'Upload document scan in order to verify your identity' }
+      : {},
     initialValues: {
       document: {
         url: undefined,
         type: undefined,
       },
     },
-    destroyOnUnmount: false,
-    persistentSubmitErrors: true,
+    destroyOnUnmount: true,
   })
 )(Document)
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,8 +1,9 @@
 import Input from './Input'
+import Modal from './Modal'
 import Button from './Button'
 import Select from './Select'
 import Captcha from './Captcha'
 import Uploader from './Uploader'
 import DateInput from './DateInput'
 
-export { Input, Select, Captcha, Button, Uploader, DateInput }
+export { Modal, Input, Select, Captcha, Button, Uploader, DateInput }
