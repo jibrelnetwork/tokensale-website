970089389c0de7330e0df412217005cda6ecd4f8
Ivan Violentov <violentoff@gmail.com>
2017-12-12 16:21:56 +0300

    Update withdraw modal

--- src/sagas/account/balance.js
+++ src/sagas/account/balance.js
@@ -3,23 +3,16 @@ import { replace } from 'react-router-redux'
 import { put, call, take } from 'redux-saga/effects'
 import { startSubmit, stopSubmit } from 'redux-form'
 
-import gtm from '../../services/gtm'
-import request from '../request'
-import { SERVER } from '../.'
 import * as actions from '../../actions'
-import { BALANCE, MODALS } from '../../constants/account'
+import { SERVER } from '../.'
+import { BALANCE } from '../../constants/account'
+import request from '../request'
+import gtm from '../../services/gtm'
 
 const FORM = 'withdraw'
 const DELAY = 60000
 
-function* requestBalance() {
-  const response = yield call(
-    request,
-    `${SERVER}/api/account/`,
-    null,
-    'get',
-    { silent: true }
-  )
+function* onBalanceResponse(response) {
   if (response.success) {
     const balance = response.data.jnt_balance
     yield put(actions.account.balance.requestSuccess(balance))
@@ -28,11 +21,9 @@ function* requestBalance() {
   }
 }
 
-function* closeWithdrawModal() {
-  yield put({
-    type: MODALS.CHANGE_STATE,
-    payload: { modalName: FORM, modalState: 'close' },
-  })
+function* requestBalance() {
+  const response = yield call(request, `${SERVER}/api/account/`, null, 'get', { silent: true })
+  yield onBalanceResponse(response)
 }
 
 export function* get() {
@@ -46,11 +37,13 @@ export function* withdraw() {
   while (true) { // eslint-disable-line fp/no-loops
     yield take(BALANCE.WITHDRAW)
     yield put(startSubmit(FORM))
+
     const response = yield call(request, `${SERVER}/api/withdraw-jnt/`, null, 'post')
+
     if (response.success) {
       yield requestBalance()
       yield put(stopSubmit(FORM))
-      yield closeWithdrawModal()
+
       gtm.pushProfileRequestWithdraw()
     } else {
       yield put(stopSubmit(FORM))
@@ -63,6 +56,7 @@ export function* changeConfirm() {
     const { payload: { operationId, token } } = yield take(BALANCE.WITHDRAW_CONFIRM)
     const data = { operation_id: operationId, token }
     const response = yield call(request, `${SERVER}/api/withdraw-jnt/confirm/`, data, 'post')
+
     if (response.success) {
       yield put(replace('/welcome/withdraw-confirm/success'))
     } else if (response.error) {
