7b65148f36793da510c22b6988a18539bba6ba53
erohin <erohin.em@gmail.com>
2017-12-26 17:31:04 +0300

    [WIP] Prototype with google proxy on virtual domains

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const path = require('path')
+const vhost = require('vhost')
 const proxy = require('express-http-proxy')
+const logger = require('morgan')
 const express = require('express')
 const interceptor = require('express-interceptor')
-const { redirectToHTTPS } = require('express-http-to-https')
+// const { redirectToHTTPS } = require('express-http-to-https')
 
-const app = express()
+// Main app
+const main = express()
+main.use(logger('dev'))
+// main.use(redirectToHTTPS([/localhost:(\d{4})/]))
 
 const replaceGoogleDomainsToProxy = (text) => text
-  .replace(/\^https:\\\/\\\/www\.gstatic\.c\.\.\?/, '\\/gstatic-proxy')
-  .replace(/https:\/\/www\.google\.com/g, '/google-proxy')
-  .replace(/https:\/\/www\.gstatic\.com/g, '/gstatic-proxy')
-  .replace(/https:\/\/apis\.google\.com/g, '/gapi-proxy')
+  .replace(/https:\/\/www\.google\.com/g, 'http://google.example.com:8080')
+  .replace(/https:\/\/www\.gstatic\.com/g, 'http://gstatic.example.com:8080')
 
 const manageProxiedRequest = (_, proxyResData) =>
   replaceGoogleDomainsToProxy(proxyResData.toString('utf8'))
@@ -20,16 +23,25 @@ const proxySettings = {
   userResDecorator: manageProxiedRequest,
 }
 
-app.use(redirectToHTTPS([/localhost:(\d{4})/]))
-app.use('/gapi-proxy', proxy('https://apis.google.com', proxySettings))
-app.use('/google-proxy', proxy('https://www.google.com', proxySettings))
-app.use('/gstatic-proxy', proxy('https://www.gstatic.com', proxySettings))
-
-app.use(
+main.use('/google', proxy('http://www.google.com', proxySettings))
+main.use('/gstatic', proxy('http://www.gstatic.com', proxySettings))
+main.use(
   interceptor((req) => ({
     isInterceptable: () => /\.js/.test(req.url),
     intercept: (body, send) => { send(replaceGoogleDomainsToProxy(body)) },
   }))
 )
-app.use(express.static(path.join(__dirname, '/build')))
-app.listen(process.env.PORT || 8080)
+main.use(express.static(path.join(__dirname, '/build')))
+
+// Redirect app
+const redirect = express()
+
+redirect.use((req, res) => {
+  res.redirect(`example.com:8080/${req.vhost[0]}`)
+})
+
+const host = express()
+host.use(vhost('example.com', main))
+host.use(vhost('*.example.com', redirect))
+
+host.listen(process.env.PORT || 8080)
