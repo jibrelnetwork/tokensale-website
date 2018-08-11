a897d2b65be6c8f2fd7b760cfd3bba813d3fec36
Michael Maslov <flexxnn@gmail.com>
2018-08-12 00:48:14 +0300

    Code clean

--- src/components/Account/Transactions/Balance.js
+++ src/components/Account/Transactions/Balance.js
@@ -4,7 +4,6 @@ import numeral from 'numeral'
 import PropTypes from 'prop-types'
 import { compose } from 'lodash/fp'
 import { connect } from 'react-redux'
-import { lifecycle } from 'recompose'
 import { translate, Interpolate } from 'react-i18next'
 
 import * as actions from '../../../actions'
@@ -45,8 +44,6 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = {
   openWithdrawModal: () => actions.account.modals.changeState('withdraw', 'open'),
-  requestStart: actions.account.balance.request,
-  requestCancel: actions.account.balance.requestCancel,
 }
 
 const enhance = compose(
@@ -54,13 +51,7 @@ const enhance = compose(
   connect(
     mapStateToProps,
     mapDispatchToProps,
-  ),
-  lifecycle({
-    /* eslint-disable fp/no-this */
-    componentDidMount() { this.props.requestStart() },
-    componentWillUnmount() { this.props.requestCancel() },
-    /* eslint-enable */
-  }),
+  )
 )
 
 export default enhance(Balance)
