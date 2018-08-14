76bc36eb68f82f2ec9936d3070566b435b7950a4
Michael Maslov <mmaslov@mera.ru>
2018-08-14 13:50:04 +0300

    kill old unused reducers folder

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import auth from './auth'
-import verify from './verify'
-import tokens from './tokens'
-import account from './account'
-
-const reducers = { auth, verify, tokens, account }
-
-export default reducers
