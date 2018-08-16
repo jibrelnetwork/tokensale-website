f341096abdaed68542a60251c3b975f7a78ea319
Michael Maslov <mmaslov@mera.ru>
2018-08-16 11:27:45 +0300

    account address change confirm fix

--- src/components/AddressChange/Request.js
+++ src/components/AddressChange/Request.js
@@ -1,35 +1,39 @@
+// @flow
+
 import React from 'react'
-import PropTypes from 'prop-types'
 import { compose } from 'lodash/fp'
 import { connect } from 'react-redux'
 import { lifecycle } from 'recompose'
 import { translate } from 'react-i18next'
+import type { TFunction } from 'react-i18next'
 
-import * as actions from '../../../actions'
+import {
+  accountAddressChangeConfirm,
+} from '../../modules'
 
-const Request = ({ t }) => (
+type Props = {
+  t: TFunction,
+}
+
+const Request = ({ t }: Props) => (
   <div className="address-change-confirm">
     <div className="img loading" />
     <p>{t('confirmations.changeAddress.request')}</p>
   </div>
 )
 
-Request.propTypes = {
-  t: PropTypes.func.isRequired,
-}
-
 const mapDispatchToProps = {
-  addressChangeConfirm: actions.account.address.changeConfirm,
+  addressChangeConfirm: accountAddressChangeConfirm,
 }
 
 const enhance = compose(
   translate(),
   connect(
-    undefined,
+    null,
     mapDispatchToProps,
   ),
   lifecycle({
-    componentWillMount() {
+    componentDidMount() {
       /* eslint-disable fp/no-this */
       const { operationId, token } = this.props.match.params
       this.props.addressChangeConfirm(operationId, token)
